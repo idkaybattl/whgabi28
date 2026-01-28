3473bc8912e6b1791bf8122f6242b758ca13f1d6
idkaybattl <moritz.fechte@gmail.com>
2026-01-28 23:23:32 +0100

    added calendear from https://github.com/jackducasse/caleandar

--- abikasse/urls.py
+++ abikasse/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.abikasse, name='abikasse'),
+    path("", views.abikasse, name="abikasse"),
+    path("calendar/", views.calendar, name="calendar"),
 ]
-
--- abikasse/views.py
+++ abikasse/views.py
@@ -5,3 +5,7 @@ from django.template import loader
 def abikasse(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
+def calendar(request):
+    template = loader.get_template('calendar.html')
+    return HttpResponse(template.render())
