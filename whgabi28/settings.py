c8b18f54f29afba9984e3aa48726fec15feb2ad0
idkaybattl <moritz.fechte@gmail.com>
2026-06-24 18:03:56 +0200

    color adjustment and moving settings to .env

--- whgabi28/settings.py
+++ whgabi28/settings.py
@@ -28,27 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
-# TODO: into env
-ALLOWED_HOSTS = [
-    "localhost",
-    "192.168.178.90",
-    "127.0.0.1",
-    "93.215.127.92",
-    "192.168.178.218",
-    "192.168.178.219",
-    "whgabi28.de",
-    "84.183.51.194",
-]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", default="*").split(",")
 
-# TODO: into env
-CSRF_TRUSTED_ORIGINS = [
-    "https://192.168.178.218",
-    "http://192.168.178.218",
-    "https://84.183.51.194",
-    "http://84.183.51.194",
-]
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", default="*").split(",")
 
 
 # Application definition
